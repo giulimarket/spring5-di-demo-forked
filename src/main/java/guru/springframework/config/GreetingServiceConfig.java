45bf9fda6e63f02ae68652157a753ba9ae0a36d1
giulimarket <giuli.market@gmail.com>
2019-05-24 11:38:39 +0200

    Lezione 82

---
+++ src/main/java/guru/springframework/config/GreetingServiceConfig.java
@@ -0,0 +1,42 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+
+@Configuration
+public class GreetingServiceConfig {
+
+	@Bean
+	GreetingServiceFactory greetingServiceFactory(GreetingRepository repository) {
+		return new GreetingServiceFactory(repository);
+	}
+	
+	@Bean
+	@Primary
+	@Profile({"default", "en"})
+	GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+		return greetingServiceFactory.createGreetingService("en");
+	}
+
+	@Bean
+	@Primary
+	@Profile("es")
+	GreetingService primarySpanishService(GreetingServiceFactory greetingServiceFactory) {
+		return greetingServiceFactory.createGreetingService("es");
+	}
+	
+	@Bean
+	@Primary
+	@Profile("de")
+	GreetingService primaryGermanService(GreetingServiceFactory greetingServiceFactory) {
+		return greetingServiceFactory.createGreetingService("de");
+	}
+	
+}
